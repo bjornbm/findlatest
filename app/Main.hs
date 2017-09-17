b28099476ef717733acc49db4013ff7a99987031
Björn Buckwalter <bjorn@buckwalter.se>
2017-09-17 17:08:00 +0900

    Rename to findlatest taking extension as arg.

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,24 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
-import Lib
 import Data.List (intercalate, nubBy, sort)
 import System.Directory
 import System.Environment
 import System.FilePath
 import System.FilePath.Find
+import System.IO
 
 main :: IO ()
-main = do
-  [dir] <- getArgs
-  -- print dir
-  files <- find (pure True) (extension ==? ".tcl") dir
+main = getArgs >>= \case
+  [dir, ext] -> findLatest dir ext
+  _          -> usage
+
+findLatest :: FilePath -> String -> IO ()
+findLatest dir ext = do
+  files <- find (pure True) (extension ==? "." ++ ext) dir
   metas <- mapM meta files
-  -- putStrLn . unwords $ (map (escape . third) . nubBy f . sort) metas
   putStr $ (intercalate "\NUL" . map third . nubBy f . reverse . sort) metas
-  -- mapM_  print $ (map third . nubBy f . sort) metas
   where
     f (_, n1, _) (_, n2, _) = n1 == n2
     third (_, _, x) = x
@@ -28,3 +31,5 @@ escape []       = []
 escape (' ':cs) = '\\' : ' ' : escape cs
 escape ( c :cs) =         c  : escape cs
 -- quoteString s = '"' : mappend s "\""
+
+usage = hPutStrLn stderr "Usage: findlatest DIR EXT"
